ca9bc8ea07b608a0b29f2a73893cb54fe6c8ade1
JDumadag25 <justin.dumadag@gmail.com>
2018-02-27 00:04:03 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+def prime?(n)
+ if n <= 0
+   return false
+ elsif n == 1
+   return false
+ elsif n == 2
+   return true
+ else
+   i = 2
+   while i < n.abs
+     if n.abs % i == 0
+       return false
+    end
+    i += 1
+   end
+  end
+ return true
+end

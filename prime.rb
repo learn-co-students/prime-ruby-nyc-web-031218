23d4ac5f83b9a555b6f0175c490cadd6e5cba4c1
Celeste Gerard <celeste.gerard@gmail.com>
2018-03-03 00:22:03 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+
+def prime?(integer)
+  divisors = (2..(integer -1)).to_a
+  if integer >= 0 && divisors.any? {|divisor| integer % divisor == 0} || integer == 0 || integer == 1
+  false
+else
+  true
+end
+end
